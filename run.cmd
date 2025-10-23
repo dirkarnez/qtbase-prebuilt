84279bcdf5dafdfa16a02b5a961530ffae929e64
dirkarnez <smalldirkalex@gmail.com>
2025-10-23 12:47:20 +0800

    - wip

---
+++ run.cmd
@@ -0,0 +1,23 @@
+@echo off
+
+@REM run as Administrator
+@echo off
+cd /d %~dp0
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+set DOWNLOADS_DIR_LINUX=%DOWNLOADS_DIR:\=/%
+
+set PYTHON_DIR=%DOWNLOADS_DIR%\python-3.10.8-amd64-portable
+
+set PATH=^
+%PYTHON_DIR%;^
+%PYTHON_DIR%\Scripts;^
+%DOWNLOADS_DIR%\PortableGit\bin;^
+%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64;^
+%DOWNLOADS_DIR%\winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64-9.0.0-r1\mingw64\bin;^
+%DOWNLOADS_DIR%\cmake-3.29.3-windows-x86_64\bin;
+
+
+
+@REM python3 -m llama_cpp.server --model <model_path_to_functionary_v2_model> --chat_format functionary-v2 --hf_pretrained_model_name_or_path <model_path_to_functionary_v2_tokenizer>
+python main.py
+pause
