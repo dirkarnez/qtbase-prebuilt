55417703a1a3fb79e719a794fbde816945caaa0e
dirkarnez <smalldirkalex@gmail.com>
2025-10-23 17:14:48 +0800

    - wip

--- local-build.cmd
+++ local-build.cmd
@@ -17,23 +17,22 @@ set PATH=^
 %DOWNLOADS_DIR%\ninja-win;^
 %DOWNLOADS_DIR%\cmake-3.29.3-windows-x86_64\bin;
 
-
 set QT_VERSION=6.10.0
 set QT_VERSION_COARSE=6.10
 
-@REM cd /d "%DOWNLOADS_DIR%\qtbase-everywhere-src-%QT_VERSION%" &&^
-@REM mkdir "cmake-build" &&^
-@REM cd "cmake-build" &&^
-@REM ..\configure.bat -static -static-runtime -release -nomake examples -prefix "installation" -opensource -confirm-license -qt-libpng -qt-libjpeg -qt-zlib -qt-pcre -qt-freetype -schannel -opengl desktop -platform win32-g++ &&^
-@REM cmake --build . --parallel --config Release &&^
-@REM cmake --install .
+cd /d "%DOWNLOADS_DIR%\qtbase-everywhere-src-%QT_VERSION%" &&^
+mkdir "cmake-build" &&^
+cd "cmake-build" &&^
+..\configure.bat -static -static-runtime -release -nomake examples -prefix "installation" -opensource -confirm-license -qt-libpng -qt-libjpeg -qt-zlib -qt-pcre -qt-freetype -schannel -opengl desktop -platform win32-g++ &&^
+cmake --build . --parallel --config Release &&^
+cmake --install .
 
-@REM cd /d "%DOWNLOADS_DIR%\qttools-everywhere-src-%QT_VERSION%" &&^
-@REM mkdir "cmake-build" &&^
-@REM cd "cmake-build" &&^
-@REM cmake ..\CMakeLists.txt -G "Ninja" -DCMAKE_INSTALL_PREFIX="%DOWNLOADS_DIR_LINUX%/qtbase-everywhere-src-%QT_VERSION%/cmake-build/installation" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="%DOWNLOADS_DIR_LINUX%/qtbase-everywhere-src-%QT_VERSION%/cmake-build/installation/lib/cmake"
-@REM cmake --build . --parallel --config Release &&^
-@REM cmake --install .
+cd /d "%DOWNLOADS_DIR%\qttools-everywhere-src-%QT_VERSION%" &&^
+mkdir "cmake-build" &&^
+cd "cmake-build" &&^
+cmake ..\CMakeLists.txt -G "Ninja" -DCMAKE_INSTALL_PREFIX="%DOWNLOADS_DIR_LINUX%/qtbase-everywhere-src-%QT_VERSION%/cmake-build/installation" -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH="%DOWNLOADS_DIR_LINUX%/qtbase-everywhere-src-%QT_VERSION%/cmake-build/installation/lib/cmake"
+cmake --build . --parallel --config Release &&^
+cmake --install .
 
 cd /d "%DOWNLOADS_DIR%\qttranslations-everywhere-src-%QT_VERSION%" &&^
 mkdir "cmake-build" &&^
